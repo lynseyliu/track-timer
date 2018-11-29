923d98779e64f5abdc041d4ac48f6a29129caa62
Lynsey Liu <lynseyliu@gmail.com>
2018-11-28 22:32:26 -0800

    things

--- TrackTimer/app/src/main/java/com/tshibley/tracktimer/MainActivity.java
+++ TrackTimer/app/src/main/java/com/tshibley/tracktimer/MainActivity.java
@@ -11,15 +11,45 @@ import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.core.Mat;
+import org.opencv.android.JavaCameraView;
 
-public class MainActivity extends Activity implements CvCameraViewListener2{
-    private CameraBridgeViewBase mOpenCvCameraView;
-    public CvCameraViewListener2 camListener;
+public class MainActivity extends Activity implements CvCameraViewListener2 {
+    private JavaCameraView cameraView;
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                    Log.i("VideoTest", "OpenCV loaded successfully");
+                    cameraView.enableView();
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.loadLibrary ("opencv_java3");
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return null;
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
     }
 }
