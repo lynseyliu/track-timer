f0ae06bf5910c2251e492d84db1a77991e07dbac
Lynsey Liu <lynseyliu@gmail.com>
2018-11-28 22:42:18 -0800

    set camera view visible maybe

--- TrackTimer/app/src/main/java/com/tshibley/tracktimer/MainActivity.java
+++ TrackTimer/app/src/main/java/com/tshibley/tracktimer/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceView;
 
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.BaseLoaderCallback;
@@ -12,6 +13,7 @@ import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.core.Mat;
 import org.opencv.android.JavaCameraView;
+
 
 public class MainActivity extends Activity implements CvCameraViewListener2 {
     private JavaCameraView cameraView;
@@ -35,6 +37,9 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cameraView = (JavaCameraView) findViewById(R.id.surface_view);
+        cameraView.setVisibility(SurfaceView.VISIBLE);
+        cameraView.setCvCameraViewListener(this);
         System.loadLibrary ("opencv_java3");
     }
 
