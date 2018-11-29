255bdbcb5ed0375b368f0614ff5b28ee53761419
Lynsey Liu <lynseyliu@gmail.com>
2018-11-28 23:17:37 -0800

    more camera things

--- TrackTimer/app/src/main/java/com/tshibley/tracktimer/MainActivity.java
+++ TrackTimer/app/src/main/java/com/tshibley/tracktimer/MainActivity.java
@@ -13,17 +13,28 @@ import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.core.Mat;
 import org.opencv.android.JavaCameraView;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 
 
 public class MainActivity extends Activity implements CvCameraViewListener2 {
     private JavaCameraView cameraView;
+    private Mat mat;
+    private final Scalar green = new Scalar(0,255,0);
+    private int resolutionIndex = 0;
+    //private IplImage videoImage = null;
+    boolean recording = false;
+    //private volatile FFmpegFrameRecorder recorder;
+    private int imageWidth = 320;
+    private int imageHeight = 240;
+    private int frameRate = 30;
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
             switch (status) {
                 case LoaderCallbackInterface.SUCCESS:
-                    Log.i("VideoTest", "OpenCV loaded successfully");
+                    Log.i("VideoTag", "OpenCV loaded successfully");
                     cameraView.enableView();
                     break;
                 default:
@@ -44,17 +55,42 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
     }
 
     @Override
+    public void onPause() {
+        super.onPause();
+        if (cameraView != null){
+            cameraView.disableView();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (cameraView != null)
+            cameraView.disableView();
+    }
+
+    @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        return inputFrame.rgba();
+        Mat rgba = inputFrame.rgba();
+        Size size = rgba.size();
+        return rgba;
     }
 
     @Override
     public void onCameraViewStarted(int width, int height) {
-
+        mat = new Mat();
     }
 
     @Override
     public void onCameraViewStopped() {
+        if (mat != null)
+            mat.release();
 
+        mat = null;
     }
 }
